245531c280c1df92c2500cf456dc10ea5e4f55a5
MafiaCoconut <alex.kach.2000@gmail.com>
2025-11-24 00:25:53 +0100

    Updated parsed_release tests

--- services/support/testing-service/tests/integration/repositories/parser/test_parsed_release_repo.py
+++ services/support/testing-service/tests/integration/repositories/parser/test_parsed_release_repo.py
@@ -7,26 +7,39 @@ from integration.common import BaseCrudRepoTest
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.usefixtures("seed_parsed_release_list")
+@pytest.mark.usefixtures("seed_source_list", "seed_parsed_release_list")
 class TestParsedReleaseRepo(BaseCrudRepoTest):
     entity_cls = ParsedRelease
     repo_attr = "parsed_release"
     sample_create_data = {
-        "name": "Ghoulia Yelps Dead Tired",
-        "characters": {"main": ["Ghoulia Yelps"]},
-        "series_name": {"name": "Dead Tired"},
-        "type_name": {"type": "doll"},
-        "gender": {"value": "female"},
-        "multi_pack": {"is_multi": False},
-        "year": {"value": 2011},
-        "description": "Ghoulia’s pajama party doll with sleep mask and zombie slippers.",
-        "primary_image": "https://example.com/images/ghoulia_dt.jpg",
-        "link": "https://monsterhigh.fandom.com/wiki/Ghoulia_Yelps_(Dead_Tired)",
-        "processing_state": ProcessingStates.INIT,
-        "source": "monsterhigh_fandom",
-        "original_html_content": "<html><body>Ghoulia data...</body></html>",
+        "name":"Clawdeen Wolf",
+        "mpn":"MPN999",
+        "year_raw":"2013",
+        "year":2013,
+        "gender_raw":"ghoul",
+        "multi_pack_raw":"single",
+        "characters_raw":["clawdeen"],
+        "series_raw":["Howleen"],
+        "type_raw":["doll"],
+        "exclusive_vendor_raw":["amazon"],
+        "pet_names_raw":["crescent"],
+        "reissue_of_raw":{"name":"none"},
+        "extra":{},
+        "primary_image":"https://img",
+        "images":{},
+        "images_link":"https://all",
+        "description_raw":"desc",
+        "from_the_box_text_raw":"box",
+        "link":"https://link",
+        "source_id":1,
+        "source_item_id":"src",
+        "original_html_content":"<h>",
+        "raw_payload":{},
+        "content_hash":"hash",
+        "processing_state":"pending",
+        "processing_error_code":None,
     }
-    unique_field = ParsedRelease.NAME
-    unique_field_value = "Ghoulia Yelps Dead Tired"
-    update_field = "processing_state"
-    updated_value = ProcessingStates.INIT
+    unique_field = "name"
+    unique_field_value = "Clawdeen Wolf"
+    update_field = "mpn"
+    updated_value = "UPDATED123"
